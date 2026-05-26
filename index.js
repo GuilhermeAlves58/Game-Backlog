2101b9a589171baea050258f533780f5b4968b58
perkles <guilhermealves5194@gmail.com>
2026-05-26 12:34:48 -0300

    routes added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import "dotenv/config";
+import gameRoutes from "./routes/games.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use("/", gameRoutes);
 
 app.listen(port, () => {
   console.log(`"Hello World! port ${port}`);
---
+++ routes/games.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.render();
+});
+
+router.get("/search", (req,res) => {
+    res.render();
+});
+
+router.post("/add", (req,res) => {
+
+});
+
+router.get("/edit/:id", (req,res) => {
+    
+});
+
+router.post("/edit/:d", (req,res) => {
+    
+});
+
+router.delete("/delete/:id", (req,res) =>{
+
+});
