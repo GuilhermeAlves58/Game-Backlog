766ec7c42be74b1b4a180cf16b251b18547587f3
perkles <guilhermealves5194@gmail.com>
2026-06-01 16:50:57 -0300

    .

--- routes/games.js
+++ routes/games.js
@@ -4,26 +4,34 @@ import axios from "axios";
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.render();
+router.get("/", (req, res) => {
+    res.render("index.ejs");
 });
 
-router.get("/search", (req,res) => {
-    res.render();
+router.get("/search", async (req, res) => {
+    try {
+        const game = req.query.q;
+        const data = await axios.get(`https://api.rawg.io/api/games?search=${game}&key=${process.env.RAWG_API_KEY}`);
+        res.json(data.data);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-router.post("/add", (req,res) => {
+router.post("/add", (req, res) => {
 
 });
 
-router.get("/edit/:id", (req,res) => {
-    
-});
-
-router.post("/edit/:d", (req,res) => {
-    
-});
-
-router.delete("/delete/:id", (req,res) =>{
+router.get("/edit/:id", (req, res) => {
 
 });
+
+router.post("/edit/:d", (req, res) => {
+
+});
+
+router.delete("/delete/:id", (req, res) => {
+
+});
+
+export default router;
